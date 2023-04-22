eb632977a0cfc5eea79409b379eb59c2a21cb871
maurirod <mauricio.rodriguez4@catolica.edu.sv>
2023-04-22 08:37:33 -0600

    Agregar archivos de proyecto.

---
+++ L02P02_2020GL602_2020RJ601/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace L02P02_2020GL602_2020RJ601.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
