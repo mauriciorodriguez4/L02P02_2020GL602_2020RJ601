79547efbd01ff6497a795834fe25642fcc070498
Gabriel_Garcia <gabriel.garcia@catolica.edu.sv>
2023-04-22 08:54:59 -0600

    Creacion Contexto de la Base

---
+++ L02P02_2020GL602_2020RJ601/Models/restauranteDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace L02P02_2020GL602_2020RJ601.Models
+{
+    public class restauranteDbContext : DbContext
+    {
+        public restauranteDbContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
