859e6c7b69e81aa3038ecaaa026e2bae13e66abd
Gabriel_Garcia <gabriel.garcia@catolica.edu.sv>
2023-04-22 09:26:25 -0600

    Adicion de CRUD de platos

---
+++ L02P02_2020GL602_2020RJ601/Models/platos.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace L02P02_2020GL602_2020RJ601.Models
+{
+    public class platos
+    {
+
+        [Key]
+        public int platoId { get; set; }
+
+        public string nombrePlato { get; set; }
+
+        public double precio { get; set; }
+    }
+}
--- L02P02_2020GL602_2020RJ601/Models/restauranteDbContext.cs
+++ L02P02_2020GL602_2020RJ601/Models/restauranteDbContext.cs
@@ -10,6 +10,8 @@ namespace L02P02_2020GL602_2020RJ601.Models
 
         public DbSet<pedidos> pedidos { get; set; }
 
+        public DbSet<platos> platos { get; set; }
+
         public DbSet<motoristas> motoristas { get; set;}
         public DbSet<clientes> clientes { get; set;}
     }
