571f6c6359f836b7969e8a30972984726765218e
Gabriel_Garcia <gabriel.garcia@catolica.edu.sv>
2023-04-22 09:32:48 -0600

    Correcion de tipos de Datos

--- L02P02_2020GL602_2020RJ601/Models/pedidos.cs
+++ L02P02_2020GL602_2020RJ601/Models/pedidos.cs
@@ -13,7 +13,7 @@ namespace L02P02_2020GL602_2020RJ601.Models
         public int clienteId { get; set; }
         public int platoId { get; set; }
         public int cantidad { get; set; }
-        public double precio { get; set; }
+        public decimal precio { get; set; }
 
     }
 }
--- L02P02_2020GL602_2020RJ601/Models/platos.cs
+++ L02P02_2020GL602_2020RJ601/Models/platos.cs
@@ -10,6 +10,6 @@ namespace L02P02_2020GL602_2020RJ601.Models
 
         public string nombrePlato { get; set; }
 
-        public double precio { get; set; }
+        public decimal precio { get; set; }
     }
 }
