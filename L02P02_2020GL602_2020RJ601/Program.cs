fb09747b87599bd4dc03f717fee04d529b1a675e
maurirod <mauricio.rodriguez4@catolica.edu.sv>
2023-04-22 08:52:24 -0600

    inyeccion de dependencias

--- L02P02_2020GL602_2020RJ601/Program.cs
+++ L02P02_2020GL602_2020RJ601/Program.cs
@@ -1,8 +1,9 @@
+using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
+builder.Services.AddDbContext<restauranteDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("blogDbConnection")));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
